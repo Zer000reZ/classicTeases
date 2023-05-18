264c179db4291cab96a4f4bc78ab756e538a9b8e
Zer000reZ <zerolelouchindex1@gmail.com>
2023-05-18 03:27:25 +0200

    added support for audio teases

--- web/script.js
+++ web/script.js
@@ -2,6 +2,7 @@ const FILE = document.getElementById("input-file");
 const TEXT = document.getElementById("text-content");
 const IMG = document.getElementById("img-content");
 const PAGE = document.getElementById('page-num');
+const CONTINUE = document.getElementById("continue");
 var TEASE;
 var old_page = 1;
 
@@ -11,9 +12,9 @@ FILE.onchange = async ()=>{
         let tt = document.getElementById("tease_title")
         tt.innerHTML = TEASE['meta']['title'] + tt.innerHTML;
         if(TEASE["meta"].hasOwnProperty("author")){
-            let author = document.getElementById("author")
-            author.innerText = TEASE['meta']['author']['name']
-            author.href = "https://milovana.com/webteases/?author=" + TEASE['meta']['author']['user-id']
+            let author = document.getElementById("author");
+            author.innerText = TEASE['meta']['author']['name'];
+            author.href = "https://milovana.com/webteases/?author=" + TEASE['meta']['author']['user-id'];
         }
     }catch{
         alert("Invalid File. Try again");
@@ -25,18 +26,30 @@ FILE.onchange = async ()=>{
 document.getElementById("overlay").style.display = "flex";
 
 function loadPage(){
-    document.getElementById("continue").innerHTML = "Go to Page";
+    CONTINUE.innerHTML = "Go to Page";
     let pagename = "page " + PAGE.value;
     if(!TEASE.hasOwnProperty(pagename)){
         alert("Page number too big");
         PAGE.value = old_Page;
         return;
     }
-    IMG.src = "https://media.milovana.com/timg/tb_l/" + TEASE[pagename]['img']
+    IMG.src = "https://media.milovana.com/timg/tb_l/" + TEASE[pagename]['img'];
     TEXT.innerHTML = TEASE[pagename]['text'];
+    if(TEASE[pagename].hasOwnProperty('audio')){
+        let audio = document.createElement('audio');
+        audio.src = "https://media.milovana.com/timg/" + TEASE[pagename]['audio'];
+        audio.setAttribute('controls', '');
+        TEXT.parentElement.insertBefore(audio, TEXT);
+    }
     if(!TEASE.hasOwnProperty("page " + (PAGE.value*1 + 1))){
-        document.getElementById("continue").innerHTML = "END<br>Go to Page";
-        PAGE.value = 1;
+        if(PAGE.value=="1"){
+            CONTINUE.innerHTML = "END";
+            CONTINUE.onclick = "";
+            PAGE.style.display = "none";
+        }else{
+            CONTINUE.innerHTML = "END<br>Go back to Page";
+            PAGE.value = 1;
+        }
     }else{
        PAGE.value = PAGE.value*1 + 1;
     }
